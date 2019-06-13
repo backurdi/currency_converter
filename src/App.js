a6c73017814f5c72508fdc2be7a9ef111c72ff34
backurdi <bachir098@gamil.com>
2019-06-13 15:11:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Currency_converter from './currency_converter/Currency_converter';
+
+class App extends Component {
+  state = {
+    currency: [],
+    result: '',
+    currency1: ''
+  }
+
+  componentDidMount() {
+    axios.get('https://data.fixer.io/api/latest?access_key=eff15d471c7ba065a8cb7ebc4672cb30')
+      .then(res => this.setState({ currency: [res.data.rates] }))
+  }
+
+  convert = (currency1, currency2, currencyValue1 ) => {
+    axios.get(`https://data.fixer.io/api/convert?access_key=eff15d471c7ba065a8cb7ebc4672cb30&from=${currency1}&to=${currency2}&amount=${currencyValue1}`)
+      .then(res => console.log(res.data))
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Currency_converter currency={this.state.currency} convert={this.convert}></Currency_converter>
+        <button onClick={ this.convert }>Convert</button>
+      </div>
+    );
+  } 
 }
 
 export default App;
